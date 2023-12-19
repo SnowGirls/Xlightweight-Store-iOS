4589403a4bde427411150a894cb1c055abc07d6b
Tesla.xpel <Xpe@Tesla.com>
2023-12-19 13:50:00 +0800

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  Xlightweight-Store-iOSTests.m
+//  Xlightweight-Store-iOSTests
+//
+//  Created by SnowGirls on 12/15/2023.
+//  Copyright (c) 2023 SnowGirls. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
